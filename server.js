76588e0212c7bc848f6e602ced1f71c84a6c405f
vebitot <vebitot@smartek21.com>
2014-11-08 21:53:03 +0530

    added registration page

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 var express = require('express');
     app = express();
 	cons = require('consolidate');
+    bodyParser = require("body-parser");
 	MongoClient = require('mongodb').MongoClient;
     Server = require('mongodb').Server;
     app.engine('html', cons.swig);
     app.set('view engine', 'html');
     app.set('views', __dirname + "/views");
-    app.use(express.bodyParser());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
     // app.use(app.router);
 // Handler for internal server error
 function errorHandler(err, req, res, next){
@@ -21,26 +23,61 @@ app.use(errorHandler);
 var mongoclient = new MongoClient(new Server('localhost',27017,{
     'native_parser' : true}))
 
-var db = mongoclient.db('students');
+//connecting to a database
+var db = mongoclient.db('chatblog');
 
+
+// setting up url
 app.get('/', function(req, res){
-        db.collection('grades').findOne({"name":"Vebito Tetseo"}, function(err, doc){
+        db.collection('users').findOne({"name":"Vebito Tetseo"}, function(err, doc){
         if (err) throw err;
         res.render("index", doc);
     })
 });
-app.get('/home/:name', function(req, res, next){
-        var name = req.params.name;
-        var getVar1 = req.query.gv1;
-        var getVar2 = req.query.gv2;
-        res.render("home", {name:name, v1:getVar1, v2:getVar2});
+app.get('/home', function(req, res, next){
+        res.render("register");
+});
+
+app.get('/login', function(req, res, next){
+        res.render("login");
 });
 
 app.post('/register',function(req, res, next){
-    var name = req.body.name;
+    var name = req.body.username;
     var email = req.body.email;
-    if
+    if(name.trim() == "" || name == null || email.trim() =="" || email == null)
+    {
+        next(Error("please enter a username and email!"));
+        console.log("nothing here");
+    }
+    else{
+        console.log(name);
+        console.log(email);
+        db.collection('users').insert({'name':name, 'email': email}, function(err, doc){
+            if(err) throw err;
+            res.render("index",doc);
+        })
+    }
 });
+
+app.post('/authenticate',function(req, res, next){
+    var username = req.body.username;
+    var password = req.body.password;
+    if(username.trim() == "" || username == null || password.trim() == "" || password == null)
+    {
+        next(Error("please enter a username and email!"));
+        console.log("nothing here");
+    }
+    else{
+        console.log(username);
+        console.log(password);
+        db.collection('users').findOne({'username':username}, function(err, doc){
+            if(err) throw err;
+            console.log(doc);
+        })
+    }
+});
+
 mongoclient.open(function(err, mongoclient){
     if (err) throw err;
     app.listen(8000);
